b46e1e1b122d5678db145bb315bde395e6703b22
RoninHietanen <84194778+RoninHietanen@users.noreply.github.com>
2021-06-13 02:46:22 +1000

    more password character creation

--- script.js
+++ script.js
@@ -1,55 +1,65 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var genBtn = document.querySelector('#generate');
+var passText = document.querySelector('#password');
+var passLength = document.querySelector('#length');
+var passUpper = document.querySelector('#uppercase');
+var passLower = document.querySelector('#lowercase');
+var passNum = document.querySelector('#numbers');
+var passSChar = document.querySelector('#specialchar');
+var getpass = document.querySelector('#getpassword');
 
 // Write password to the #password input
-function generatePassword() {
-  var specialCharacters = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
-  var upperCaseLetters = Math.random().toString(36).slice(2);
-  var lowerCaseLetters = Math.random().toString(36).slice(2);
-  var passwordText = document.querySelector("#password");
-  var numbers = Math.floor(Math.random());
-
-  if () {
-    
+let arrayLow2High = (low, high) => {
+  var array = [];
+  for (let i = low; i <= high; i++){
+    array.push(i);
   }
-}
+  return array;
+};
 
-console.log(generatePassword())
+var uppercase = arrayLow2High(65, 90);
+var lowercase = arrayLow2High(97, 122);
+var num = arrayLow2High(48, 57);
+var schar = arrayLow2High(33, 47).concat(arrayLow2High(58, 64)).concat(arrayLow2High(91, 96)).concat(arrayLow2High(123, 126));
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword());
+getpass.addEventListener('submit', (e) => {
+  e.preventDefault();
+  var characterAmount = passLength.value;
+  var includeUppercase = passUpper.checked;
+  var includeLowercase = passLower.checked;
+  var includeNumbers = passNum.checked;
+  var includeSymbols = passSChar.checked;
+  var password = genPass(
+    characterAmount,
+    includeUppercase,
+    includeLowercase,
+    includeNumbers,
+    includeSymbols
+  );
+  passText.innerText = password;
+});
+
+let genPass = (
+  characterAmount,
+  includeUppercase,
+  includeLowercase,
+  includeNumbers,
+  includeSymbols
+) => {}
+
+if (includeUppercase) charCodes = charCodes.concat(uppercase);
+if (includeLowercase) charCodes = charCodes.concat(lowercase);
+if (includeSymbols) charCodes = charCodes.concat(num);
+if (includeNumbers) charCodes = charCodes.concat(schar);
 
 
 
-//javascript:(
-//  function(){
-//      prompt('Here is your shiny new password:', 
-//          Math.random().toString(36).slice(2) + 
-//          Math.random().toString(36).slice(2)
-//      );
-//  }
-//)();
 
-//function password_generator( len ) {
-//  var length = (len)?(len):(10);
-//  var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
-//  var numeric = '0123456789';
-//  var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
-//  var password = "";
-//  var character = "";
-//  var crunch = true;
-//  while( password.length<length ) {
-//      entity1 = Math.ceil(string.length * Math.random()*Math.random());
-//      entity2 = Math.ceil(numeric.length * Math.random()*Math.random());
-//      entity3 = Math.ceil(punctuation.length * Math.random()*Math.random());
-//      hold = string.charAt( entity1 );
-//      hold = (password.length%2==0)?(hold.toUpperCase()):(hold);
-//      character += hold;
-//      character += numeric.charAt( entity2 );
-//      character += punctuation.charAt( entity3 );
-//      password = character;
-//  }
-//  password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
-//  return password.substr(0,len);
+//var specialCharacters = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+//var upperCaseLetters = Math.random().toString(36).slice(2);
+//var lowerCaseLetters = Math.random().toString(36).slice(2);
+//var numbers = Math.floor(Math.random());
+//for (i = 0; i < writePassword.length; i++) {
+//  document.writePassword(generatePassword[i]).maxLength;
+//  document.writePassword(writePassword()[i]).minLength;
 //}
-//console.log( password_generator() );
